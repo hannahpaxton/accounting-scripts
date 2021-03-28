9796f52458918e61ccf028a8284a970d92f81b18
Hannah Paxton <hannahlinnpaxton@gmail.com>
2021-03-28 17:29:58 -0400

    Completed melon dictionary and script

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,37 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+# from melons import melon_names, melon_seedlessness, melon_prices
 
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
+# def print_melon(name, seedless, price):
+#     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+from melons import melon_dictionary
+
+def print_melon(melon_dictionary):
+        "Print each melon with corresponding attribute information"
+
+for melon_name, info in melon_dictionary.items():
+    print('')
+    print(melon_name.upper())
+
+    for info, attributes in info.items():
+        print(f'{info}: {attributes}')
+
+print_melon(melon_dictionary)
+
+
+
+
+
--- melons.py
+++ melons.py
@@ -1,23 +1,29 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# } 
+
+melon_dictionary = {'Honeydew': {'seedless': True, 'price': .99, 'flesh_color': None, 'weight': None, 'rind_color': None},
+                    'Crenshaw': {'seedless': False, 'price': 2.00, 'flesh_color': None, 'weight': None, 'rind_color': None},
+                    'Crane': {'seedless': False, 'price': 2.50, 'flesh_color': None, 'weight': None, 'rind_color': None},
+                    'Casaba': {'seedless': False, 'price': 2.50, 'flesh_color': None, 'weight': None, 'rind_color': None},
+                    'Cantaloupe': {'seedless': False, 'price': .99, 'flesh_color': None, 'weight': None, 'rind_color': None}}
